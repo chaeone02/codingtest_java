9b80d943fe578070065ac98b580c670378eaa06d
Chaewon Lee <112858933+chaeone02@users.noreply.github.com>
2026-07-29 08:40:12 +0000

    SWEA/D3/4192/수영대회

--- swea/4192.swimming2.java
+++ swea/4192.swimming2.java
@@ -1,135 +1,73 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-
-    static int startR, startC;
-    static int endR, endC;
-
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-
-    static class Node {
-        int r;
-        int c;
-        int time;
-
-        Node(int r, int c, int time) {
-            this.r = r;
-            this.c = c;
-            this.time = time;
-        }
-    }
-
-    public static void main(String args[]) throws Exception {
-
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-
-        for (int tc = 1; tc <= T; tc++) {
-
-            // 수영장 크기
-            N = sc.nextInt();
-
-            map = new int[N][N];
-            visited = new boolean[N][N];
-
-            // 수영장 지도 입력
-            for (int r = 0; r < N; r++) {
-                for (int c = 0; c < N; c++) {
-                    map[r][c] = sc.nextInt();
-                }
-            }
-
-            // 시작 위치
-            startR = sc.nextInt();
-            startC = sc.nextInt();
-
-            // 도착 위치
-            endR = sc.nextInt();
-            endC = sc.nextInt();
-
-            int answer = bfs();
-
-            System.out.println("#" + tc + " " + answer);
-        }
-
-        sc.close();
-    }
-
-    static int bfs() {
-
-        Queue<Node> queue = new ArrayDeque<>();
-
-        queue.offer(new Node(startR, startC, 0));
-        visited[startR][startC] = true;
-
-        while (!queue.isEmpty()) {
-
-            Node cur = queue.poll();
-
-            // 도착점에 도착했다면 현재 시간 반환
-            if (cur.r == endR && cur.c == endC) {
-                return cur.time;
-            }
-
-            boolean wait = false;
-
-            // 상하좌우 탐색
-            for (int d = 0; d < 4; d++) {
-
-                int nr = cur.r + dr[d];
-                int nc = cur.c + dc[d];
-                int nextTime = cur.time + 1;
-
-                // 수영장 범위를 벗어남
-                if (nr < 0 || nr >= N || nc < 0 || nc >= N) {
-                    continue;
-                }
-
-                // 장애물
-                if (map[nr][nc] == 1) {
-                    continue;
-                }
-
-                // 이미 방문한 위치
-                if (visited[nr][nc]) {
-                    continue;
-                }
-
-                // 일반 물
-                if (map[nr][nc] == 0) {
-
-                    visited[nr][nc] = true;
-                    queue.offer(new Node(nr, nc, nextTime));
-                }
-
-                // 소용돌이
-                else if (map[nr][nc] == 2) {
-
-                    // 소용돌이가 사라진 시간에만 이동
-                    if (nextTime % 3 == 0) {
-
-                        visited[nr][nc] = true;
-                        queue.offer(new Node(nr, nc, nextTime));
-
-                    } else {
-                        // 현재 위치에서 기다릴 필요가 있음
-                        wait = true;
-                    }
-                }
-            }
-
-            // 소용돌이 때문에 이동하지 못한 경우 현재 위치에서 1초 대기
-            if (wait) {
-                queue.offer(new Node(cur.r, cur.c, cur.time + 1));
-            }
-        }
-
-        return -1;
-    }
+	static int T;
+	static int N;
+	static int[][] maps;
+	static int A,B,C,D;
+	static int result;
+	
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine().trim());
+		for (int tc = 1; tc <= N; tc++) {
+			N = Integer.parseInt(br.readLine().trim());
+			maps = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				// 한 줄의 공백을 입력받는다
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					maps[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			C = Integer.parseInt(st.nextToken());
+			D = Integer.parseInt(st.nextToken());
+			
+			result = bfs();
+			System.out.println("#" + tc + " " + result);
+			
+		}
+	}
+	
+	static int bfs() {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {A,B});
+		maps[A][B] = 1;
+		
+		while (!queue.isEmpty()) {
+			int[] p = queue.poll();
+			
+			int r = p[0];
+			int c = p[1];
+			
+			if (r == C && c == D) 
+				return maps[r][c] - 1;
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				
+				if(!check(nr, nc)) continue;
+				
+				//0인 곳만 이동가능, 1은 장애물, 2이상은 이미 방문
+				if (maps[nr][nc] != 0) continue;
+				maps[nr][nc] = maps[r][c] + 1;
+				queue.offer(new int[] {nr, nc});
+			}
+		}
+		return -1;
+	}
+	
+	static boolean check(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
+	}
 }
